ca70d352d9dd5ad7f1241a871d28492f7dd904ea
hcarolina <carolina.46032@gmail.com>
2021-01-06 16:40:21 -0500

    2/4 crud functions

--- src/cli.py
+++ src/cli.py
@@ -1,65 +1,75 @@
 """
-Work wants an inventory app that: 
-    Stores data into a file
-    Uses the commad line to list/add/update/delete:
+Work wants an inventory app that:
+    Stores Data into a file
+    Uses the command line to list/add/update/delete:
         "Items" they have:
-            id 
+            id
             name
             cond
 """
+from models.item import Item
+
 next_id = 0
-items = [1, 2, 3]
-# TODO make a menu print out showing options
+items = []
+# TODO Make a menu print out showing options
 def menu():
     print("""
-    1. List All Items 
-    2. Add New Item
-    3. Update Exisitng Item
-    4. Delete Item (By item id)
-    5. Exit
-    """)
+1. List All Items
+2. Add New Item
+3. Update Existing Item
+4. Delete Item (By item id)
+5. Exit
+""")
 
-#List all Items
+# List all Items
 def list_items():
     for item in items:
         print(item)
-    print("in list item function")
-#Add New Item
+
+# Add New Item
 def new_item():
     global next_id
+    global items
+
     name = input("Name: ")
     cond = input("Condition: ")
     item_id = next_id
-    next_id += 1
-    
 
-#Update Exisitng Item
+    next_id += 1
+
+    tmp = Item(item_id, name, cond)
+    items.append(tmp)
+
+
+# Update Existing Item
 def update_existing(itemId):
     pass
 
-#Delete Item(By item id)
+# Delete Item (By item id)
 def delete_item(itemId):
     pass
 
-# Make the menu questions that grab the data
-while True:
-    menu()
-    choice = (input("> "))
-        
-    if choice == "1":
-        list_items()
-    elif choice == "2":
-        new_item()
-    elif choice == "3":
-        pass
-    elif choice == "4":
-        pass
-    elif choice == "5": # Exit
-        exit()
-    else:
-        input("Invalid Input!\n(Press Enter to try again")
+# Make the menu questions that grab the data 
+def main():
+    while True:
+        menu()
+        choice = input("> ")
+
+        if choice == "1":
+            list_items()
+        elif choice == "2":
+            new_item()
+        elif choice == "3":
+            pass
+        elif choice == "4":
+            pass
+        elif choice == "5": # Exit
+            exit()
+        else:
+            input("Invalid Input!\n(Press Enter to try again)")
 
 
-# Make the File saving stuff
+# Make the File Saving stuff
 
-
+if __name__ == "__main__":
+    main()
